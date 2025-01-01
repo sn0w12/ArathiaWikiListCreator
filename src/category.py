25bdc7a62c424b8c4720f61e2200be11ee866b01
sn0w12 <lucasskold49@gmail.com>
2025-01-01 15:46:11 +0100

    Delete category.py

--- src/category.py
+++
@@ -1,8 +0,0 @@
-class Category:
-    def __init__(self, name, subcategories=None):
-        self.name = name
-        self.subcategories = subcategories or {}
-        self.entries = []
-
-    def add_entry(self, entry):
-        self.entries.append(entry)
